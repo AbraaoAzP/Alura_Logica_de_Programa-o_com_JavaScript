ded047577a147eaf5b16184cfd8d7e7ac46fa06f
AbraaoAzP <abraaoazp@gmail.com>
2024-03-04 20:54:37 -0300

    Aprender sobre Alert, Prompt e If

---
+++ app.js
@@ -0,0 +1,7 @@
+alert("Boas vindas ao jogo do número secreto!");
+let numeroSecreto = 5;
+let chute = prompt("Escolha um número entra 1 e 10");
+
+if (chute == numeroSecreto) {
+    alert(`Parabéns! Você acertou o número secreto! (${numeroSecreto})`);
+}
