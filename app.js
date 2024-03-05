1fc215dfcd8d451d32e51c44a7c33b090b4751b5
AbraaoAzP <abraaoazp@gmail.com>
2024-03-05 00:42:13 -0300

    Aprender sobre break, Operador Ternário, Math.random e parseInt

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 alert("Boas vindas ao jogo do número secreto!");
-let numeroSecreto = 7;
+let numeroMaximo = 100;
+let numeroSecreto = parseInt((Math.random() * numeroMaximo) + 1);
 let chute;
 let tentativas = 1;
 
 while(chute != numeroSecreto) {
-    chute = prompt("Escolha um número entra 1 e 10");
+    chute = prompt(`Escolha um número entra 1 e ${numeroMaximo}`);
 
     if (chute == numeroSecreto) {
-        alert(`Parabéns! Você acertou o número secreto (${numeroSecreto}) com ${tentativas} tentativas!`);
+        break;
     } else {
         if (chute < numeroSecreto) {
             alert(`O número secreto é maior que ${chute}`);
@@ -16,3 +17,7 @@ while(chute != numeroSecreto) {
         }
     }   tentativas++;
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+
+alert(`Parabéns! Você acertou o número secreto (${numeroSecreto}) com ${tentativas} ${palavraTentativa}!`);
