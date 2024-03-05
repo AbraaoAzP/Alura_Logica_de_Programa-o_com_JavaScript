e1976620f5b5caf5a1c29fe973c7ec45aab0cf35
AbraaoAzP <abraaoazp@gmail.com>
2024-03-05 00:02:25 -0300

    Aprender sobre If, Else, Else If e While

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 alert("Boas vindas ao jogo do número secreto!");
 let numeroSecreto = 7;
-let chute = prompt("Escolha um número entra 1 e 10");
+let chute;
+let tentativas = 1;
 
-if (chute == numeroSecreto) {
-    alert(`Parabéns! Você acertou o número secreto! (${numeroSecreto})`);
-} else {
-    alert("Que pena! Você errou :(");
+while(chute != numeroSecreto) {
+    chute = prompt("Escolha um número entra 1 e 10");
+
+    if (chute == numeroSecreto) {
+        alert(`Parabéns! Você acertou o número secreto (${numeroSecreto}) com ${tentativas} tentativas!`);
+    } else {
+        if (chute < numeroSecreto) {
+            alert(`O número secreto é maior que ${chute}`);
+        } else {
+            alert(`O número secreto é menor que ${chute}`);
+        }
+    }   tentativas++;
 }
