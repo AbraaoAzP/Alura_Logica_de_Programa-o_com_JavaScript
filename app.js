18c340122d4749d25c9dc7c9326405a32ae32507
AbraaoAzP <abraaoazp@gmail.com>
2024-03-04 21:21:32 -0300

    Aprender sobre else, concatenação e Live Server

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 alert("Boas vindas ao jogo do número secreto!");
-let numeroSecreto = 5;
+let numeroSecreto = 7;
 let chute = prompt("Escolha um número entra 1 e 10");
 
 if (chute == numeroSecreto) {
     alert(`Parabéns! Você acertou o número secreto! (${numeroSecreto})`);
+} else {
+    alert("Que pena! Você errou :(");
 }
